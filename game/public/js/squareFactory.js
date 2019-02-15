652a2c437416c909e3bbd97d158daf9f05c82235
william.li <william.li@tbs.cn>
2019-02-15 09:41:41 +0800

    改变了方块的旋转方向

--- game/public/js/squareFactory.js
+++ game/public/js/squareFactory.js
@@ -1,97 +1,55 @@
-const Square = function(){
-    // 方块数据
-    this.data = [
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-    ]
-
-    // 原点
-    this.origin = {
-        x: 0,
-        y: 0
+class Square {
+    constructor() {
+        // 方块数据
+        this.data = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ];
     }
-
-    // 方向
-    this.dir = 0;
-    
-    // 旋转状态的数组
-    this.rotates = [
-        [
-            [0, 2, 0, 0],
-            [0, 2, 0, 0],
-            [0, 2, 0, 0],
-            [0, 2, 0, 0],
-        ],
-        [
+    canRotate(isValid) {
+        let dir = (this.dir + 1) % 4;
+        var test = [
             [0, 0, 0, 0],
             [0, 0, 0, 0],
-            [2, 2, 2, 2],
             [0, 0, 0, 0],
-        ],
-        [
-            [0, 2, 0, 0],
-            [0, 2, 0, 0],
-            [0, 2, 0, 0],
-            [0, 2, 0, 0],
-        ],
-        [
-            [0, 0, 0, 0],
-            [0, 0, 0, 0],
-            [2, 2, 2, 2],
-            [0, 0, 0, 0],
-        ],
-    ]
-}
-Square.prototype.canRotate = function(isValid) {
-    let dir  = (this.dir + 1) % 4;
-    
-    var test = [
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]
-    ];
-
-    test = JSON.parse(JSON.stringify(this.rotates[dir]))
-
-    return isValid(this.origin, test);
-}
-Square.prototype.canDown = function(isValid) {
-    const test = {};
-    test.x = this.origin.x + 1;
-    test.y = this.origin.y;
-
-    return isValid(test, this.data);
-}
-Square.prototype.canLeft = function(isValid){
-    const test = {};
-    test.x = this.origin.x;
-    test.y = this.origin.y - 1;
-
-    return isValid(test, this.data);
-}
-Square.prototype.canRight = function(isValid){
-    const test = {};
-    test.x = this.origin.x;
-    test.y = this.origin.y + 1;
-
-    return isValid(test, this.data);
-}
-Square.prototype.down = function(){
-    this.origin.x = this.origin.x + 1;
-}
-Square.prototype.left = function(){
-    this.origin.y = this.origin.y - 1;
-}
-Square.prototype.right = function(){
-    this.origin.y = this.origin.y + 1;
-}
-Square.prototype.rotate = function(num = 0) {
-    this.dir = (this.dir + 1 + num) % 4;
-
-    this.data = JSON.parse(JSON.stringify(this.rotates[this.dir]));
+            [0, 0, 0, 0]
+        ];
+        test = JSON.parse(JSON.stringify(this.rotates[dir]));
+        return isValid(this.origin, test);
+    }
+    canDown(isValid) {
+        const test = {};
+        test.x = this.origin.x + 1;
+        test.y = this.origin.y;
+        return isValid(test, this.data);
+    }
+    canLeft(isValid) {
+        const test = {};
+        test.x = this.origin.x;
+        test.y = this.origin.y - 1;
+        return isValid(test, this.data);
+    }
+    canRight(isValid) {
+        const test = {};
+        test.x = this.origin.x;
+        test.y = this.origin.y + 1;
+        return isValid(test, this.data);
+    }
+    down() {
+        this.origin.x = this.origin.x + 1;
+    }
+    left() {
+        this.origin.y = this.origin.y - 1;
+    }
+    right() {
+        this.origin.y = this.origin.y + 1;
+    }
+    rotate(num = 0) {
+        this.dir = (this.dir + 1 + num) % 4;
+        this.data = JSON.parse(JSON.stringify(this.rotates[this.dir]));
+    }
 }
 // I
 class Square1 extends Square{
@@ -141,9 +99,9 @@ class Square2 extends Square{
                 [0, 0, 0, 0],
             ],
             [
-                [2, 0, 0, 0],
+                [0, 2, 0, 0],
                 [2, 2, 0, 0],
-                [2, 0, 0, 0],
+                [0, 2, 0, 0],
                 [0, 0, 0, 0],
             ],
             [
@@ -153,9 +111,9 @@ class Square2 extends Square{
                 [0, 0, 0, 0],
             ],
             [
-                [0, 2, 0, 0],
+                [2, 0, 0, 0],
                 [2, 2, 0, 0],
-                [0, 2, 0, 0],
+                [2, 0, 0, 0],
                 [0, 0, 0, 0],
             ],
         ];
@@ -176,8 +134,8 @@ class Square3 extends Square{
                 [0, 0, 0, 0],
             ],
             [
-                [2, 0, 0, 0],
                 [2, 2, 2, 0],
+                [0, 0, 2, 0],
                 [0, 0, 0, 0],
                 [0, 0, 0, 0],
             ],
@@ -188,8 +146,8 @@ class Square3 extends Square{
                 [0, 0, 0, 0],
             ],
             [
+                [2, 0, 0, 0],
                 [2, 2, 2, 0],
-                [0, 0, 2, 0],
                 [0, 0, 0, 0],
                 [0, 0, 0, 0],
             ],
@@ -210,8 +168,8 @@ class Square4 extends Square{
                 [0, 0, 0, 0],
             ],
             [
+                [0, 0, 2, 0],
                 [2, 2, 2, 0],
-                [2, 0, 0, 0],
                 [0, 0, 0, 0],
                 [0, 0, 0, 0],
             ],
@@ -222,8 +180,8 @@ class Square4 extends Square{
                 [0, 0, 0, 0],
             ],
             [
-                [0, 0, 2, 0],
                 [2, 2, 2, 0],
+                [2, 0, 0, 0],
                 [0, 0, 0, 0],
                 [0, 0, 0, 0],
             ],
@@ -331,42 +289,6 @@ class Square7 extends Square{
         ];
     }
     
-}
-
-class SquareFactory{
-    make(index, dir){
-        let square;
-        switch(index){
-            case 1:
-                square = new Square1();
-                break;
-            case 2:
-                square = new Square2();
-                break;
-            case 3:
-                square = new Square3();
-                break;
-            case 4:
-                square = new Square4();
-                break;
-            case 5:
-                square = new Square5();
-                break;
-            case 6:
-                square = new Square6();
-                break;
-            case 7:
-                square = new Square7();
-                break;
-            default:
-                break;
-        }
-        square.origin.x = 0;
-        square.origin.y = 4;
-        square.rotate(dir);
-
-        return square;
-    }
 }
 
 const makeSquare = (index, dir) => {
